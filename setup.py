b0c0f56f2f6498fff3e65b8efa322a14f4ae4f75
DenitsaKostova <dkostova93@gmail.com>
2016-07-06 10:44:43 +0300

    rearrange some modules adding setup module

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="Bookoholic",
+    description="A desktop app for bookoholics",
+    long_description=open("README.md").read(),
     version="0.1",
-    packages=find_packages(),
-    install_requires=['PyQt>=5.2.1'],
     author="Denitsa Petrova",
     author_email="dkostova93@gmail.com",
-    description="A desktop app for bookoholics",
+    install_requires=['PyQt>=5.2.1'],
+    packages=['goodreads'],
     license="GNU",
     keywords="book library goodreads",
     url="https://github.com/DenitsaKostova/Bookoholic"
